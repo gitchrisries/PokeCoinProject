0ae38fe136b7719320de4c00213ce87fcd3a5750
gitchrisries <christophries1@student.hs-rm.de>
2022-12-13 23:35:22 +0100

    added: features and code improvements
    new features:
     - Get and show Wallet Balance
     - Stopwatch for hashing time
     - Stop Mining if Tab not in focus

--- src/components/Mining.jsx
+++ src/components/Mining.jsx
@@ -1,9 +1,10 @@
 import React, {useEffect} from 'react'
+import {useStopwatch} from 'react-timer-hook';
 
-async function getPreviousHash() {
+
+async function getLastBlock() {
     try {
-        const response = await fetch('http://localhost:3000/blockchain/lastBlock')
-        return await response.json()
+        return (await fetch('http://localhost:3000/blockchain/lastBlock')).json()
     } catch (error) {
         console.log(error)
     }
@@ -24,63 +25,111 @@ async function postNewBlock(block) {
     console.log('block:', JSON.stringify(block))
 
     try {
-        const response = await fetch('http://localhost:3000/blockchain/blocks', requestOptions)
-        return response
+        return await fetch('http://localhost:3000/blockchain/blocks', requestOptions)
     } catch (error) {
         console.log(error)
     }
 }
 
+async function getWalletBalance() {
+    const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6IkNocmlzIiwiaWF0IjoxNjcwODkzMDYzLCJleHAiOjE2NzA5Nzk0NjN9.vt7Dj_ADOjhQYP370AmMSEtMrkaRVouhPuPfKFZBuDo"
 
-let worker = null
-
-function Mining() {
-    const [newHash, setNewHash] = React.useState('')
-    worker?.terminate()
-    worker = new Worker(new URL('../helpers/worker.js', import.meta.url));
-
-    async function runMining(prevHash) {
-        worker.onmessage = async (message) => {
-            let time = (new Date()).toLocaleTimeString()
-            console.log(`New Hash found: (${time}) `, message.data.newHash)
-            const prom = new Promise(async (resolve, reject) => {
-                postNewBlock(message.data.newBlock).then(
-                    (response) => {
-                        resolve(response)
-                    },
-                    (error) => {
-                        reject(error)
-                    },
-                )
-            });
-            await prom
-            setNewHash(message.data.newHash)
+    const requestOptions = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'token': token
         }
-        worker.postMessage({previousHash: prevHash, difficulty: 5})
     }
 
-    const startMining = async () => {
-        const _prevHash = await getPreviousHash().then(json => json.hash)
-        runMining(_prevHash).catch(console.log)
+    try {
+        return (await fetch('http://localhost:3000/wallet/balance', requestOptions)).json()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+function WalletBalance() {
+    const [walletBalance, setWalletBalance] = React.useState(0)
+
+    async function getBalance() {
+        let newBalance = (await getWalletBalance()).amount
+        setWalletBalance(newBalance)
     }
 
-    useEffect(() => {
-        startMining().catch(console.log)
-    },[worker])
-
+    getBalance().catch(console.log)
 
     return (
-        <div>
-            <p>New Hash found: {newHash}</p>
-        </div>
+        <div>Wallet Balance: {walletBalance}</div>
     )
 }
 
+
+let worker = null
+
 function MiningPage() {
+    const [newHash, setNewHash] = React.useState('')
+    const [miningStatus, setMiningStatus] = React.useState(true)
+
+    async function runMining() {
+        const _prevHash = (await getLastBlock()).hash
+        worker.postMessage({previousHash: _prevHash, difficulty: 5})
+
+        worker.onmessage = (message) => {
+            if (!miningStatus) return
+            postNewBlock(message.data.newBlock).then(() => {
+                setNewHash(message.data.newHash)
+            })
+        }
+    }
+
+    const startMining = async () => {
+        await runMining()
+    }
+
+    document.addEventListener('visibilitychange', function () {
+        if (document.hidden) {
+            setMiningStatus(false)
+        } else {
+            setMiningStatus(true)
+        }
+    });
+
+    useEffect(() => {
+        worker?.terminate()
+        if (miningStatus) {
+            worker = new Worker(new URL('../helpers/worker.js', import.meta.url));
+            startMining().catch(console.log)
+        }
+    }, [worker, miningStatus])
+
+    function MyStopwatch() {
+        const {seconds} = useStopwatch({autoStart: true});
+
+        return (
+            <div style={{fontSize: '20px', marginBottom: 5}}>
+                Last hash {miningStatus && seconds}s ago
+            </div>
+        )
+    }
+
     return (
-        <div className="App"
-             style={{textAlign: 'center', borderRadius: 10, backgroundColor: 'gray', padding: 2, margin: 5}}>
-            {Mining()}
+        <div style={{textAlign: 'center'}}>
+            <div style={{
+                textAlign: 'center',
+                borderRadius: 10,
+                backgroundColor: 'gray',
+                padding: 10,
+                margin: '100px 500px 10px 500px'
+            }}>
+                <WalletBalance/>
+                {newHash && <p>New hash found: {newHash}</p>}
+                {miningStatus ? <p>miningStatus: True</p> : <p>miningStatus: False</p>}
+                <MyStopwatch/>
+            </div>
+            <div>
+                <button onClick={() => setMiningStatus(!miningStatus)}>RUN/STOP</button>
+            </div>
         </div>
     );
 }
