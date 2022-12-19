4513c5180a6d045cd5ee45d9ecbcf93a7e584461
kinaIen <ina.kien@student.hs-rm.de>
2022-12-19 10:34:19 +0100

    userapi eingebunden

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,12 +1,14 @@
 import React, {useState, createContext, useContext} from "react";
-import {Box, Input, HStack, VStack, InputGroup, InputLeftAddon, Button} from '@chakra-ui/react'
+import { Box, Input, HStack, VStack, InputGroup, InputLeftAddon, Button } from '@chakra-ui/react';
+import ApiClient from "../clients/pokecoin/src/ApiClient";
+import {UsersApi} from "../clients/pokecoin/src";
 
 const url = 'http://localhost:3000'
 const UserContext = createContext({});
 
-function RegisterLoginButton({path,name}) {
-    const loginData = useContext(UserContext);
-    const handleRegister = React.useCallback(() => {
+function RegisterLoginButton({path, name}) {
+    const opts = {'body': useContext(UserContext)};
+    /*const handleRegister = React.useCallback(() => {
         const requestOptions = {
             method: 'POST',
             headers: {
@@ -20,7 +22,16 @@ function RegisterLoginButton({path,name}) {
             .then(result => console.log(result))
             .catch(error => console.log('error', error));
 
-    });
+    });*/
+    const callback1 = (error,data,response) => {
+        console.log('resp: '+response.text);
+        console.log('data: '+data.username);
+        console.log('error: '+error);
+    };
+    const handleRegister = () => {
+        const apiClient = new ApiClient(url);
+        console.log(new UsersApi(apiClient).authRegisterPost(opts,callback1));
+    };
     return (
         <Button onClick={handleRegister}>{`${name}`}</Button>
     );
@@ -38,16 +49,16 @@ function Login() {
 
     return (
         <UserContext.Provider value={loginData}>
-            <VStack spacing={10} align='left'>
+            <VStack spacing={8} align='left' ml={'5'}>
                 <Box>
                     <h2 align='left'>Login</h2>
                 </Box>
                 <InputGroup>
-                    <InputLeftAddon children='Username' width='35%'/>
+                    <InputLeftAddon children='Username' width='5%'/>
                     <Input type='text' value={loginData.username} onChange={onChangeUserName}/>
                 </InputGroup>
                 <InputGroup>
-                    <InputLeftAddon children='Password' width='35%'/>
+                    <InputLeftAddon children='Password' width='5%'/>
                     <Input type='password' value={loginData.password} onChange={onChangePassword}/>
                 </InputGroup>
                 <HStack>
