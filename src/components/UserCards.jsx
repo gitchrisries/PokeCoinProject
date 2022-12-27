20573dac9ef2b17ac010ff92c2e3288e94e459db
kinaIen <ina.kien@student.hs-rm.de>
2022-12-27 11:57:50 +0100

    UserCards angepasst, Abfrage funktioniert jetzt

--- src/components/UserCards.jsx
+++ src/components/UserCards.jsx
@@ -1,69 +1,69 @@
 import _apiClient from "../helpers/globals";
 import {CardsApi} from "../clients/pokecoin/src";
 import {useQueries, useQuery, useQueryClient} from "@tanstack/react-query";
-import {useState} from "react";
-import {Flex, Table, Tr, Td, Tbody} from "@chakra-ui/react"
+import React,{useState} from "react";
+import {Grid,GridItem,Text} from "@chakra-ui/react"
 
 const cardApi = new CardsApi(_apiClient)
 
-function CardRow({cardId, name}) {
-    return (
-        <Tr color={'white'}>
-            <Td>{cardId}</Td>
-            <Td>{name}</Td>
-        </Tr>
-    );
-}
-
 function UserCards() {
-    const [userCards, setUserCards] = useState([])
-    const queryClient = useQueryClient()
+    const [uniqueUserCards, setUniqueUserCards] = useState(null)
+    const allCards = React.useRef({})
 
-    const {isLoading, error, data} = useQuery(['usercards'],
+    const {data: userCards} = useQuery(['userCards'],
         async () => {
-            const response = await cardApi.cardsUsercardsGet();
-            setUserCards(response.map((card) => {
-                return {cardId: card.cardId}
-            }))
-            //console.log(response);
-            return response;
-        })
+            return await cardApi.cardsUsercardsGet();
+        }, {
+            onSuccess: () => {
+                const _cardCountDict = {}
+                userCards?.forEach((obj) => {
+                    _cardCountDict[obj.cardId] = (_cardCountDict[obj.cardId] || 0) + 1
+                })
+                setUniqueUserCards(_cardCountDict)
+            }
+        }
+    )
 
-    const cardQueries = useQueries({
-        queries: userCards.map(card => {
-            return {
-                queryKey: ['cardById', card.cardId],
-                queryFn: () => async() => {
-                    const response = await cardApi.cardsCardIdGet(card.cardId);
-                    console.log(response)
-                    return response;
+    //Was ist daran besser als mehrere useQuery?
+    //Sollen wir davon ausgehen, dass wir die Pageanzahl wissen?
+    const allCardsQueries = useQueries({
+        queries: [
+            {
+                queryKey: ['cards', 0], queryFn: async () => {
+                    const response = await cardApi.cardsGet({page:0})
+                    response.cards.forEach(card => allCards.current[card.id] = card)
+                    return response
+                }
+            },
+            {
+                queryKey: ['cards', 1], queryFn: async () => {
+                    const response = await cardApi.cardsGet({page:1})
+                    response.cards.forEach(card => allCards.current[card.id] = card)
+                    return response
+                }
+            },
+            {
+                queryKey: ['cards', 2], queryFn: async () => {
+                    const response = await cardApi.cardsGet({page:2})
+                    response.cards.forEach(card => allCards.current[card.id] = card)
+                    return response
                 }
             }
-        })
-    });
+        ]
+    })
 
-    console.log(cardQueries)
-
-    if (isLoading) {
-        return (
-            <div>Loading...</div>
-        )
-    }
     return (
-        <Flex direction="column" w="100%">
-            <Table>
-                <Tbody>
-                    {userCards.map((row) => {
-                        return (
-                            <CardRow
-                                cardId={row.cardId}
-                                key={row.cardId}
-                                name={row.name}
-                            />
-                        )
-                    })}</Tbody>
-            </Table>
-        </Flex>
+        <Grid templateColumns='repeat(4, 1fr)' gap={6}>
+            {uniqueUserCards && allCardsQueries.every(resp => resp.status === 'success') &&
+                Object.entries(uniqueUserCards).map((entry) => {
+                    //entry[0] = cardId, entry[1] = amount
+                    return <GridItem w='100%' h='100%' key={entry[0]}>
+                        <img src={allCards.current[entry[0]].imageUrl} alt={entry[0]}/>
+                        <Text color={'white'}>{allCards.current[entry[0]].name}</Text>
+                        <Text color={'white'}>Amount: {entry[1]}</Text>
+                    </GridItem>
+                })}
+        </Grid>
     );
 }
 
