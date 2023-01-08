3d4cd8b63d016133f6d479478d7392d40d55a9b3
kinaIen <ina.kien@student.hs-rm.de>
2023-01-08 19:31:46 +0100

    NUR ZWISCHENSPEICHER; Bitte keine Zeit in korrektur investieren

--- src/pages/UserFunctions.jsx
+++ src/pages/UserFunctions.jsx
@@ -1,10 +1,11 @@
 import React, {useContext} from "react";
 import {LoggedContext} from "../contexts/LoggedContext";
-import {useDisclosure, HStack} from "@chakra-ui/react";
+import {useDisclosure, HStack, MenuButton, MenuList, Menu, MenuItem, MenuDivider, Avatar} from "@chakra-ui/react";
 import {Button} from "@mantine/core";
 import LoginModal2 from "./LoginModal2";
 import {UsersApi} from "../clients/pokecoin/src";
 import {_apiClient} from "../helpers/globals";
+import {UnlockIcon, RepeatIcon, AddIcon, CloseIcon} from "@chakra-ui/icons";
 
 const userApi = new UsersApi(_apiClient)
 
@@ -15,13 +16,23 @@ function UserFunctions(){
     const {isOpen: isOpenPw, onOpen: onOpenPw, onClose: onClosePw} = useDisclosure()
 
     return(
-        <HStack>
-            {!loggedIn && <><Button onClick={onOpenLogin}>Login</Button> <Button onClick={onOpenReg}>Register</Button></>}
-            {loggedIn && <Button onClick={onOpenPw}>Change Password</Button>}
-            {isOpenLogin && <LoginModal2 isOpen={isOpenLogin} onClose={onCloseLogin} buttonName={'Login'} option={'login'}/>}
-            {isOpenReg && <LoginModal2 isOpen={isOpenReg} onClose={onCloseReg} buttonName={'Register'} option={'register'}/>}
-            {isOpenPw && <LoginModal2 isOpen={isOpenPw} onClose={onClosePw} buttonName={'Change Password'} option={'changePassword'}/>}
-        </HStack>
+        <Menu>
+            <MenuButton as={Avatar} bg='#0398fc' >
+            </MenuButton>
+            <MenuList >
+                {!loggedIn  ? <>
+                <MenuItem icon={<UnlockIcon/>} onClick={onOpenLogin}>Login</MenuItem>
+                <MenuItem icon={<AddIcon/>} onClick={onOpenReg}>Register</MenuItem>
+                </> : <>
+                <MenuItem bg='#0398fc' icon={<RepeatIcon/>} onClick={onOpenPw}>Change Password</MenuItem>
+                    <MenuDivider />
+                    <MenuItem icon={<CloseIcon/>}>Logout</MenuItem>
+                </>}
+                {isOpenLogin && <LoginModal2 isOpen={isOpenLogin} onClose={onCloseLogin} buttonName={'Login'} option={'login'}/>}
+                {isOpenReg && <LoginModal2 isOpen={isOpenReg} onClose={onCloseReg} buttonName={'Register'} option={'register'}/>}
+                {isOpenPw && <LoginModal2 isOpen={isOpenPw} onClose={onClosePw} buttonName={'Change Password'} option={'changePassword'}/>}
+            </MenuList>
+        </Menu>
     );
 }
 
